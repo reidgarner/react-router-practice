f7d930a0e3ed0fdbf2894bfd253535c903e0e052
Matt Winzer <matt.winzer@gmail.com>
2018-12-13 10:36:22 -0700

    added Navigation components

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import NotFound from './components/NotFound'
+import Navigation from './components/Navigation'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navigation />
         <Home />
         <About />
         <Contact />
---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div>
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/contact">Contact</a>
+    </div>
+  )
+}
+
+export default Navigation
