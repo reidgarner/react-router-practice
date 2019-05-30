f537618486e94945efdb53c1719928f14c4a0347
reidgarner <reidbgarner@gmail.com>
2019-05-30 15:53:25 -0600

    404 redirect to home working

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 
-const NotFound = () => {
-  return (
-    <div>
-      <h1>404 - Not Found</h1>
-      <p>Whoops, you seem to be lost. You can find your way home by clicking below:</p>
-      <a href='/'>Go Home</a>
-    </div>
-  )
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>404 - Not Found</h1>
+        <p>Whoops, you seem to be lost. You can find your way home by clicking below:</p>
+        <Redirect to='/' />
+      </div>
+    )
+  }
 }
 
 export default NotFound
